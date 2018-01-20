41a3bee04c0452cd80215d8ee4fe12a9349085dc
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-20 21:49:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', function(key) {
+  $('#typing').on('keydown', function(key) {
     if(key.which === 'G') {
       alert('Someone pressed the G key!!!');
     }
